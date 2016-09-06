7ab4281534086b3e2cd0b599c327aa652e41decc
zlfkyo <zlfkyo@sina.com>
2016-09-06 15:20:36 +0800

    j

--- src/main/java/chapter03/t03_inst/sizeof/DeepSizeOfMain.java
+++ src/main/java/chapter03/t03_inst/sizeof/DeepSizeOfMain.java
@@ -3,9 +3,9 @@ package chapter03.t03_inst.sizeof;
 public class DeepSizeOfMain {
 
 	public static void main(String []args) {
-		DeepObjectSizeOf.deepSizeOf(new Integer(1));
-		DeepObjectSizeOf.deepSizeOf(new String());
-		DeepObjectSizeOf.deepSizeOf(new char[1]);
+		System.out.println(DeepObjectSizeOf.deepSizeOf(new Integer(1)));
+		System.out.println(DeepObjectSizeOf.deepSizeOf(new String()));
+		System.out.println(DeepObjectSizeOf.deepSizeOf(new char[1]));
 	}
 }
 
--- src/main/java/chapter03/t03_inst/sizeof/NormalSizeOfTestMain.java
+++ src/main/java/chapter03/t03_inst/sizeof/NormalSizeOfTestMain.java
@@ -6,10 +6,10 @@ import java.util.HashMap;
 public class NormalSizeOfTestMain {
 
 	public static void main(String []args) {
-		System.out.println("integer:"+NormalObjectSizeOf.sizeOf(new Integer(1))/8+" byte");
-		System.out.println("map:"+NormalObjectSizeOf.sizeOf(new HashMap<String,Object>())/8+" byte");
-		System.out.println("arraylist:"+NormalObjectSizeOf.sizeOf(new ArrayList<Object>())/8+" byte");
-		System.out.println("string:"+NormalObjectSizeOf.sizeOf(new String())/8+" byte");
-		System.out.println("char[1]:"+NormalObjectSizeOf.sizeOf(new char[1])/8+" byte");
+		System.out.println("integer:"+NormalObjectSizeOf.sizeOf(new Integer(1))+" bit");
+		System.out.println("map:"+NormalObjectSizeOf.sizeOf(new HashMap<String,Object>())+" bit");
+		System.out.println("arraylist:"+NormalObjectSizeOf.sizeOf(new ArrayList<Object>())+" bit");
+		System.out.println("string:"+NormalObjectSizeOf.sizeOf(new String())+" bit");
+		System.out.println("char[1]:"+NormalObjectSizeOf.sizeOf(new char[1])+" bit");
 	}
 }
